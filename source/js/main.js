c333e6ae9360a5ae038f3402fdccab45bc590136
lilgikki <katy.kostyleva2010@gmail.com>
2022-11-18 21:01:19 +0300

    added map and fixed

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,8 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {activateMenu} from './menu.js';
 import {submitFormBooking, getMask} from './booking.js';
+import {yandexMap} from './map.js';
+import {scrollTo} from './scroll.js';
 
 // ---------------------------------
 
@@ -18,6 +20,9 @@ window.addEventListener('DOMContentLoaded', () => {
   activateMenu();
   submitFormBooking();
   getMask();
+
+  ymaps.ready(yandexMap);
+  scrollTo();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
